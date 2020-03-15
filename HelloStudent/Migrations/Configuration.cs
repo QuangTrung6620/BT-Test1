9f8bbbb438f67c50ee78e2defef9d85ad936b4eb
QuangTrung6620 <50941864+QuangTrung6620@users.noreply.github.com>
2020-03-15 20:33:10 +0700

    Test1

---
+++ HelloStudent/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+﻿namespace HelloStudent.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<HelloStudent.Models.MyDb>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(HelloStudent.Models.MyDb context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
---
+++ HelloStudent/Models/MyDb.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace HelloStudent.Models
+{
+    public class MyDb : DbContext
+    {
+        public MyDb() : base("MyConnectionString")
+        {
+        }
+
+        public System.Data.Entity.DbSet<HelloStudent.Models.Student> Students { get; set; }
+    }
+}
